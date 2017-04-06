6f44d23b29a33280b1ddb5c58680d47c7c9624ab
Michael Anthony Knyszek <mknyszek@gmail.com>
2017-04-06 16:32:34 -0700

    Parser for basic commands, comments, is working

--- src/main.rs
+++ src/main.rs
@@ -3,189 +3,41 @@
 extern crate pest;
 
 mod ast;
+mod parser;
 
-use ast::*;
-
-use std::collections::LinkedList;
+use parser::Rdp;
 
 use pest::prelude::*;
 
-impl_rdp! {
-    grammar! {
-        // Program is made up of statements
-        program = { stmt* }
+use std::io::{self, Read};
 
-        // Statements end with semi-colon
-        stmt = { func_stmt | while_stmt | var_stmt | expr_stmt }
-
-        // Types of statements
-        func_stmt  = { ["func"] ~ iden ~ iden_list ~ (expr ~ [";"] | block_expr) }
-        var_stmt   = { ["var"] ~ iden ~ ["="] ~ expr ~ [";"] }
-        while_stmt = { ["while"] ~ ["("] ~ expr ~ [")"] ~ (expr ~ [";"] | block_expr) }
-        expr_stmt  = { expr ~ [";"] }
-
-        // Most everything else is an expression
-        expr = _{
-            { ["("] ~ expr ~ [")"] | if_expr | block_expr | call_expr | assign_expr | unary_expr | snum | iden }
-            //chng = { cat }
-            lgc  = { and | or }
-            cond = { lt | gt | eq | ne }
-            sum  = { plus  | minus }
-            prod = { times | slash }
-            bit  = { band | bor | bxor }
-        }
-
-        // Operators for matching later
-        plus  =  { ["+"] }
-        minus =  { ["-"] }
-        times =  { ["*"] }
-        slash =  { ["/"] }
-        lt    =  { ["<"] }
-        gt    =  { [">"] }
-        eq    =  { ["=="] }
-        ne    =  { ["!="] }
-        and   =  { ["and"] }
-        or    =  { ["or"] }
-        not   =  { ["not"] }
-        band  =  { ["&"] }
-        bor   =  { ["|"] }
-        bxor  =  { ["^"] }
-        bnot  =  { ["~"] }
-        //cat   =  { ["><"] }
-
-        // Expressions to match
-        if_expr     = { ["if"] ~ ["("] ~ expr ~ [")"] ~ expr ~ ["else"] ~ expr }
-        block_expr  = { ["{"] ~ stmt* ~ expr ~ ["}"] }
-        call_expr   = { iden ~ ["("] ~ (arg ~ ([","] ~ arg)*)? ~ [")"] }
-        assign_expr = { iden ~ ["="] ~ expr }
-        //take_expr   = { iden ~ ["["] ~ expr ~ ["]"] }
-        //slice_expr  = { iden ~ ["["] ~ expr ~ [":"] ~ expr ~ ["]"] }
-        //alloc_expr  = { ["|"] ~ ["["] ~ expr ~ ["]"] ~ [">"] }
-        unary_expr  = { (not | bnot) ~ expr }
-
-        // Helper rules
-        arg       = { expr }
-        iden_list = _{ ["("] ~ (iden ~ ([","] ~ iden)*)? ~ [")"] }
-
-        // Atomic rules
-        iden       = @{ (['a'..'z'] | ['A'..'Z'] | ["_"]) ~ (['a'..'z'] | ['A'..'Z'] | ["_"] | ['0'..'9'])* } 
-        snum       = @{ ["0"] | (["-"]? ~ ['1'..'9'] ~ ['0'..'9']*) }
-
-        // Ignore whitespace
-        whitespace = _{ [" "] | ["\n"] | ["\r"] | ["\t"] }
+fn print_line_from_pos(program: &String, pos: usize) {
+    let mut line_start = pos;
+    let mut line_end = pos;
+    let chars: Vec<_> = program.chars().collect();
+    println!("{:?}", chars);
+    while line_start > 0 && chars[line_start] != '\n' {
+        line_start -= 1;
     }
-
-    process! {
-        parse(&self) -> Program {
-            (_: program, stmts: _stmt_list()) => stmts
-        }
-        _iden_list(&self) -> LinkedList<Iden> {
-            (&head: iden, mut rest: _iden_list()) => {
-                rest.push_front(head.parse::<String>().unwrap());
-                rest
-            },
-            () => LinkedList::new()
-        }
-        _stmt(&self) -> Stmt {
-            (_: func_stmt, &name: iden, params: _iden_list(), body: _expr()) => {
-                Stmt::DefFunc(name.parse::<String>().unwrap(), params, body)
-            },
-            (_: var_stmt, &i: iden, e: _expr()) => Stmt::DefVar(i.parse::<String>().unwrap(), e),
-            (_: while_stmt, pred: _expr(), body: _expr()) => Stmt::While(pred, body),
-            (_: expr_stmt, e: _expr()) => Stmt::Expr(e),
-        }
-        _stmt_list(&self) -> LinkedList<Stmt> {
-            (_: stmt, head: _stmt(), mut rest: _stmt_list()) => {
-                rest.push_front(head);
-                rest
-            },
-            () => LinkedList::new()
-        }
-        _expr(&self) -> Expr {
-            (&i: iden) => Expr::Ref(i.parse::<String>().unwrap()),
-            (&num: snum) => Expr::Num(num.parse::<i64>().unwrap()),
-            (_: if_expr, pred: _expr(), then: _expr(), other: _expr()) => {
-                Expr::If(Box::new(pred), Box::new(then), Box::new(other))
-            },
-            (_: block_expr, stmts: _stmt_list(), result: _expr()) => {
-                Expr::Block(stmts, Box::new(result))
-            },
-            (_: call_expr, &func: iden, args: _arg_list()) => {
-                Expr::Call(func.parse::<String>().unwrap(), args)
-            },
-            (_: assign_expr, &var: iden, value: _expr()) => {
-                Expr::Assign(var.parse::<String>().unwrap(), Box::new(value))
-            },
-            (_: unary_expr, op, e: _expr()) => {
-                Expr::UnOp(match op.rule {
-                    Rule::not => UnOp::Not,
-                    Rule::bnot => UnOp::BNot,
-                    _ => unreachable!(),
-                }, Box::new(e))
-            },
-            (_: lgc, e1: _expr(), op, e2: _expr()) => {
-                Expr::BinOp(Box::new(e1), match op.rule {
-                    Rule::and => BinOp::And,
-                    Rule::or => BinOp::Or,
-                    _ => unreachable!(),
-                }, Box::new(e2))
-            },
-            (_: cond, e1: _expr(), op, e2: _expr()) => {
-                Expr::BinOp(Box::new(e1), match op.rule {
-                    Rule::lt => BinOp::Lt,
-                    Rule::gt => BinOp::Gt,
-                    Rule::eq => BinOp::Eq,
-                    Rule::ne => BinOp::Ne,
-                    _ => unreachable!(),
-                }, Box::new(e2))
-            },
-            (_: sum, e1: _expr(), op, e2: _expr()) => {
-                Expr::BinOp(Box::new(e1), match op.rule {
-                    Rule::plus => BinOp::Add,
-                    Rule::minus => BinOp::Sub,
-                    _ => unreachable!(),
-                }, Box::new(e2))
-            },
-            (_: prod, e1: _expr(), op, e2: _expr()) => {
-                Expr::BinOp(Box::new(e1), match op.rule {
-                    Rule::times => BinOp::Mul,
-                    Rule::slash => BinOp::Div,
-                    _ => unreachable!(),
-                }, Box::new(e2))
-            },
-            (_: bit, e1: _expr(), op, e2: _expr()) => {
-                Expr::BinOp(Box::new(e1), match op.rule {
-                    Rule::band => BinOp::BAnd,
-                    Rule::bor => BinOp::BOr,
-                    Rule::bxor => BinOp::BXor,
-                    _ => unreachable!(),
-                }, Box::new(e2))
-            },
-        }
-        _arg_list(&self) -> LinkedList<Expr> {
-            (_: arg, head: _expr(), mut rest: _arg_list()) => {
-                rest.push_front(head);
-                rest
-            },
-            () => LinkedList::new()
-        }
+    while line_end < chars.len()-1 && chars[line_end] != '\n' {
+        line_end += 1;
     }
+    println!("{}", &chars[line_start..line_end].iter().cloned().collect::<String>());
+    println!("{marker:>width$}", marker = '^', width = pos - line_start);
 }
 
 fn main() {
-    let s = "var x = 0; func f() { x + 1 } f(); x;";
-    let mut parser = Rdp::new(StringInput::new(s));
-    assert!(parser.program());
-    if !parser.end() {
+    let mut buffer = String::new();
+    let _ = io::stdin().read_to_string(&mut buffer).unwrap();
+    let mut parser = Rdp::new(StringInput::new(buffer.as_str()));
+    if !parser.program() || !parser.end() {
         let (rules, pos) = parser.expected();
-        println!("{}", s);
-        println!("{marker:>width$}", marker = '^', width = pos);
+        print_line_from_pos(&buffer, pos);
         print!("[Error] Expected one of: ");
         for rule in rules.iter() {
             print!("{:?} ", rule);
         }
         println!();
     }
-    println!("{:?}", parser.queue());
     println!("{:?}", parser.parse());
 }
---
+++ src/parser.rs
@@ -0,0 +1,173 @@
+
+use ast::*;
+use pest::prelude::*;
+
+use std::collections::LinkedList;
+
+impl_rdp! {
+    grammar! {
+        // Program is made up of statements
+        program = { soi ~ stmt* ~ eoi }
+
+        // Statements end with semi-colon
+        stmt = { func_stmt | while_stmt | var_stmt | expr_stmt }
+
+        // Types of statements
+        func_stmt  = { ["func"] ~ iden ~ iden_list ~ (expr ~ [";"] | block_expr) }
+        var_stmt   = { ["var"] ~ iden ~ ["="] ~ expr ~ [";"] }
+        while_stmt = { ["while"] ~ ["("] ~ expr ~ [")"] ~ (expr ~ [";"] | block_expr) }
+        expr_stmt  = { expr ~ [";"] }
+
+        // Most everything else is an expression
+        expr = _{
+            { ["("] ~ expr ~ [")"] | if_expr | block_expr | call_expr | assign_expr | unary_expr | snum | iden }
+            //chng = { cat }
+            lgc  = { and | or }
+            cond = { lt | gt | eq | ne }
+            sum  = { plus  | minus }
+            prod = { times | slash }
+            bit  = { band | bor | bxor }
+        }
+
+        // Operators for matching later
+        plus  =  { ["+"] }
+        minus =  { ["-"] }
+        times =  { ["*"] }
+        slash =  { ["/"] }
+        lt    =  { ["<"] }
+        gt    =  { [">"] }
+        eq    =  { ["=="] }
+        ne    =  { ["!="] }
+        and   =  { ["and"] }
+        or    =  { ["or"] }
+        not   =  { ["not"] }
+        band  =  { ["&"] }
+        bor   =  { ["|"] }
+        bxor  =  { ["^"] }
+        bnot  =  { ["~"] }
+        //cat   =  { ["><"] }
+
+        // Expressions to match
+        if_expr     = { ["if"] ~ ["("] ~ expr ~ [")"] ~ expr ~ ["else"] ~ expr }
+        block_expr  = { ["{"] ~ stmt* ~ expr ~ ["}"] }
+        call_expr   = { iden ~ ["("] ~ (arg ~ ([","] ~ arg)*)? ~ [")"] }
+        assign_expr = { iden ~ ["="] ~ expr }
+        //take_expr   = { iden ~ ["["] ~ expr ~ ["]"] }
+        //slice_expr  = { iden ~ ["["] ~ expr ~ [":"] ~ expr ~ ["]"] }
+        //alloc_expr  = { ["|"] ~ ["["] ~ expr ~ ["]"] ~ [">"] }
+        unary_expr  = { (not | bnot) ~ expr }
+
+        // Helper rules
+        arg       = { expr }
+        iden_list = _{ ["("] ~ (iden ~ ([","] ~ iden)*)? ~ [")"] }
+
+        // Atomic rules
+        iden       = @{ (['a'..'z'] | ['A'..'Z'] | ["_"]) ~ (['a'..'z'] | ['A'..'Z'] | ["_"] | ['0'..'9'])* } 
+        snum       = @{ ["0"] | (["-"]? ~ ['1'..'9'] ~ ['0'..'9']*) }
+
+        // Ignore whitespace
+        whitespace = _{ [" "] | ["\n"] | ["\r"] | ["\t"] }
+
+        // Ignore comment
+        comment = _{
+              ["/*"] ~ (!(["/"] | ["*"]) ~ any)* ~ ["*/"] 
+            | ["//"] ~ (!(["\r"] | ["\n"]) ~ any)* ~ (["\n"] | ["\r\n"] | ["\r"] | eoi) 
+        }
+    }
+
+    process! {
+        parse(&self) -> Program {
+            (_: program, stmts: _stmt_list()) => stmts
+        }
+        _iden_list(&self) -> LinkedList<Iden> {
+            (&head: iden, mut rest: _iden_list()) => {
+                rest.push_front(head.parse::<String>().unwrap());
+                rest
+            },
+            () => LinkedList::new()
+        }
+        _stmt(&self) -> Stmt {
+            (_: func_stmt, &name: iden, params: _iden_list(), body: _expr()) => {
+                Stmt::DefFunc(name.parse::<String>().unwrap(), params, body)
+            },
+            (_: var_stmt, &i: iden, e: _expr()) => Stmt::DefVar(i.parse::<String>().unwrap(), e),
+            (_: while_stmt, pred: _expr(), body: _expr()) => Stmt::While(pred, body),
+            (_: expr_stmt, e: _expr()) => Stmt::Expr(e),
+        }
+        _stmt_list(&self) -> LinkedList<Stmt> {
+            (_: stmt, head: _stmt(), mut rest: _stmt_list()) => {
+                rest.push_front(head);
+                rest
+            },
+            () => LinkedList::new()
+        }
+        _expr(&self) -> Expr {
+            (&i: iden) => Expr::Ref(i.parse::<String>().unwrap()),
+            (&num: snum) => Expr::Num(num.parse::<i64>().unwrap()),
+            (_: if_expr, pred: _expr(), then: _expr(), other: _expr()) => {
+                Expr::If(Box::new(pred), Box::new(then), Box::new(other))
+            },
+            (_: block_expr, stmts: _stmt_list(), result: _expr()) => {
+                Expr::Block(stmts, Box::new(result))
+            },
+            (_: call_expr, &func: iden, args: _arg_list()) => {
+                Expr::Call(func.parse::<String>().unwrap(), args)
+            },
+            (_: assign_expr, &var: iden, value: _expr()) => {
+                Expr::Assign(var.parse::<String>().unwrap(), Box::new(value))
+            },
+            (_: unary_expr, op, e: _expr()) => {
+                Expr::UnOp(match op.rule {
+                    Rule::not => UnOp::Not,
+                    Rule::bnot => UnOp::BNot,
+                    _ => unreachable!(),
+                }, Box::new(e))
+            },
+            (_: lgc, e1: _expr(), op, e2: _expr()) => {
+                Expr::BinOp(Box::new(e1), match op.rule {
+                    Rule::and => BinOp::And,
+                    Rule::or => BinOp::Or,
+                    _ => unreachable!(),
+                }, Box::new(e2))
+            },
+            (_: cond, e1: _expr(), op, e2: _expr()) => {
+                Expr::BinOp(Box::new(e1), match op.rule {
+                    Rule::lt => BinOp::Lt,
+                    Rule::gt => BinOp::Gt,
+                    Rule::eq => BinOp::Eq,
+                    Rule::ne => BinOp::Ne,
+                    _ => unreachable!(),
+                }, Box::new(e2))
+            },
+            (_: sum, e1: _expr(), op, e2: _expr()) => {
+                Expr::BinOp(Box::new(e1), match op.rule {
+                    Rule::plus => BinOp::Add,
+                    Rule::minus => BinOp::Sub,
+                    _ => unreachable!(),
+                }, Box::new(e2))
+            },
+            (_: prod, e1: _expr(), op, e2: _expr()) => {
+                Expr::BinOp(Box::new(e1), match op.rule {
+                    Rule::times => BinOp::Mul,
+                    Rule::slash => BinOp::Div,
+                    _ => unreachable!(),
+                }, Box::new(e2))
+            },
+            (_: bit, e1: _expr(), op, e2: _expr()) => {
+                Expr::BinOp(Box::new(e1), match op.rule {
+                    Rule::band => BinOp::BAnd,
+                    Rule::bor => BinOp::BOr,
+                    Rule::bxor => BinOp::BXor,
+                    _ => unreachable!(),
+                }, Box::new(e2))
+            },
+        }
+        _arg_list(&self) -> LinkedList<Expr> {
+            (_: arg, head: _expr(), mut rest: _arg_list()) => {
+                rest.push_front(head);
+                rest
+            },
+            () => LinkedList::new()
+        }
+    }
+}
